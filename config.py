a3dedf8b2fabad2350ae3dbd83a7c7e377188a51
tony <technicalprogram4658@gmail.com>
2025-02-26 17:03:05 +0300

    Updated env variables validation logic
    Added more checks
    Removed exception handling
    Added func that prints message to stderr and stops program with exit code 1

--- config.py
+++ config.py
@@ -7,32 +7,34 @@ from cloud_services import ChecksService
 load_dotenv()
 
 
+def print_to_stderr(msg: str) -> None:
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+
 def get_env_variable(var_name: str) -> str:
     value = os.getenv(var_name)
     if not value:
-        raise EnvironmentError(f"Environment variable '{var_name}' wasn't found!")
+        print_to_stderr(f"Env variable '{var_name}' wasn't found!")
     return value
 
 
-try:
-    LOCAL_DIR_PATH = get_env_variable("LOCAL_DIR_PATH")
-    try:
-        ChecksService.check_dir(LOCAL_DIR_PATH)
-    except NotADirectoryError as not_dir_err:
-        raise EnvironmentError(f"Env variable: {'LOCAL_DIR_PATH'} {not_dir_err}")
-    except FileNotFoundError as not_found_err:
-        raise EnvironmentError(f"Env variable: {'LOCAL_DIR_PATH'} {not_found_err}")
+LOCAL_DIR_PATH = get_env_variable("LOCAL_DIR_PATH")
+if not os.path.exists(LOCAL_DIR_PATH):
+    print_to_stderr(f"Env variable 'LOCAL_DIR_PATH' is invalid.\nNo such directory: '{LOCAL_DIR_PATH}'")
+elif not os.path.isdir(LOCAL_DIR_PATH):
+    print_to_stderr(f"Env variable 'LOCAL_DIR_PATH' is invalid.\nMust be a path to directory: '{LOCAL_DIR_PATH}'.")
 
-    CLOUD_TOKEN = get_env_variable("CLOUD_TOKEN")
-    if not ChecksService.check_token(CLOUD_TOKEN, url="https://cloud-api.yandex.net/v1/disk/resources"):
-        raise EnvironmentError(f"Env variable: {'CLOUD_TOKEN'} is invalid! Check your API key.")
+CLOUD_TOKEN = get_env_variable("CLOUD_TOKEN")
+if not ChecksService.check_token(CLOUD_TOKEN, url="https://cloud-api.yandex.net/v1/disk/resources"):
+    print_to_stderr(f"Env variable: 'CLOUD_TOKEN' is invalid! Check your API key.")
 
-    CLOUD_DIR_NAME = get_env_variable("CLOUD_DIR_NAME")
-    SYNC_PERIOD = get_env_variable("SYNC_PERIOD")
-    LOG_FILE_PATH = get_env_variable("LOG_FILE_PATH")
-except FileNotFoundError as err:
-    print(err, file=sys.stderr)
-    exit(1)
-except EnvironmentError as err:
-    print(err, file=sys.stderr)
-    exit(1)
+SYNC_PERIOD = get_env_variable("SYNC_PERIOD")
+if not SYNC_PERIOD.isdigit() or int(SYNC_PERIOD) <= 0:
+    print_to_stderr(f"Env variable 'SYNC_PERIOD' variable must be int and greater than 0!")
+
+LOG_FILE_PATH = get_env_variable("LOG_FILE_PATH")
+if os.path.isdir(LOG_FILE_PATH) or not LOG_FILE_PATH.endswith(".log"):
+    print_to_stderr("Env variable 'LOG_FILE_PATH' must be a file with '.log' extension.")
+
+CLOUD_DIR_NAME = get_env_variable("CLOUD_DIR_NAME")
